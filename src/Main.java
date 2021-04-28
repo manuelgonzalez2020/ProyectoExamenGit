62202b3b811eff89370aa517e1e26d8cfe85f348
Manuel González <manuelgonzalezvela@cdpjosecabrera.es>
2021-04-28 11:54:34 +0200

    Siguiente linea
    Aquí he descrito que es donde voy a hacer las pruebas de convertir en GitHub

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Este es mi proyecto GitHub");
+		
+		System.out.println("Esta va a ser la pruba para convertir en GitHub");
 	}
 
 }

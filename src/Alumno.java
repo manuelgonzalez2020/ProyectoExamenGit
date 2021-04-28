020368ae4cf9bfb5a805ca301192306acbefb350
Manuel González <manuelgonzalezvela@cdpjosecabrera.es>
2021-04-28 11:58:49 +0200

    Creación de clase

---
+++ src/Alumno.java
@@ -0,0 +1,10 @@
+
+public class Alumno {
+
+	public Alumno() {
+		// TODO Auto-generated constructor stub
+		System.out.println("El alumno se llama Antonio");
+		System.out.println("Tiene 19 años");
+	}
+
+}
